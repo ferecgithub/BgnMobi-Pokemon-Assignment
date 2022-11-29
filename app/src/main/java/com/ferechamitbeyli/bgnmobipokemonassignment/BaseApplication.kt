d797c35f2d77c8a408db2669e73c1d34fb5aa2e4
ferecgithub <ferechamitbeyli@gmail.com>
2022-11-29 12:55:54 +0300

    BaseApplication created

---
+++ app/src/main/java/com/ferechamitbeyli/bgnmobipokemonassignment/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.ferechamitbeyli.bgnmobipokemonassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application()
--- app/src/main/java/com/ferechamitbeyli/bgnmobipokemonassignment/MainActivity.kt
+++ app/src/main/java/com/ferechamitbeyli/bgnmobipokemonassignment/MainActivity.kt
@@ -2,7 +2,10 @@ package com.ferechamitbeyli.bgnmobipokemonassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ferechamitbeyli.bgnmobipokemonassignment.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
