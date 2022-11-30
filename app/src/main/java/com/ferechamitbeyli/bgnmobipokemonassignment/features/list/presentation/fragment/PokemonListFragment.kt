1a8e6efd98ec06dfa17034bf1a7df9a1868738c1
ferecgithub <ferechamitbeyli@gmail.com>
2022-11-30 11:55:46 +0300

    Implemented view visibility change mechanism according to LoadState of pagination.

--- app/src/main/java/com/ferechamitbeyli/bgnmobipokemonassignment/features/list/presentation/fragment/PokemonListFragment.kt
+++ app/src/main/java/com/ferechamitbeyli/bgnmobipokemonassignment/features/list/presentation/fragment/PokemonListFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast.makeText
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ferechamitbeyli.bgnmobipokemonassignment.R
 import com.ferechamitbeyli.bgnmobipokemonassignment.core.common.util.State
@@ -17,6 +21,7 @@ import com.ferechamitbeyli.bgnmobipokemonassignment.databinding.FragmentOverlayP
 import com.ferechamitbeyli.bgnmobipokemonassignment.databinding.FragmentPokemonListBinding
 import com.ferechamitbeyli.bgnmobipokemonassignment.features.list.presentation.adapter.PokemonListAdapter
 import com.ferechamitbeyli.bgnmobipokemonassignment.features.list.presentation.viewmodel.PokemonListViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -47,12 +52,23 @@ class PokemonListFragment : Fragment() {
     }
 
     private fun setOnClickListeners() {
-
+        binding.layoutPokemonListError.buttonLayoutErrorTryAgain.setOnClickListener {
+            pokemonListAdapter.retry()
+        }
     }
 
     private fun setUpUI() {
         binding.recyclerViewPokemonList.apply {
             adapter = pokemonListAdapter
+            pokemonListAdapter.addLoadStateListener { loadState ->
+                binding.recyclerViewPokemonList.isVisible =
+                    loadState.source.refresh is LoadState.NotLoading
+                binding.progressBarPokemonList.isVisible =
+                    loadState.source.refresh is LoadState.Loading
+                binding.layoutPokemonListError.root.isVisible =
+                    loadState.source.refresh is LoadState.Error
+                displayError(loadState)
+            }
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
         }
@@ -79,5 +95,13 @@ class PokemonListFragment : Fragment() {
         }
     }
 
+    private fun displayError(loadState: CombinedLoadStates) {
+        val errorState = loadState.source.append as? LoadState.Error
+            ?: loadState.source.prepend as? LoadState.Error
+
+        errorState?.let {
+            Snackbar.make(binding.root, "${it.error}", Snackbar.LENGTH_LONG).show()
+        }
+    }
 
 }
